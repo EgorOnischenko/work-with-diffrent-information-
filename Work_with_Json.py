67790aaa9dcec0dff2a056455790e85365530eb0
EgorOnischenko <75452718+EgorOnischenko@users.noreply.github.com>
2021-01-07 09:09:43 +0100

    Update Work_with_Json.py

--- Work_with_Json.py
+++ Work_with_Json.py
@@ -4,36 +4,56 @@
 # In[ ]:
 
 
-import json
+def read_files(name):
 
-with open('text') as f:
-  json_data = json.load(f)
+    import json
+    import chardet
+ 
+    with open('text', 'rb') as f:
+        data = f.read()
+        result = chardet.detect(data)
+        data = data.decode(result['encoding'])
+        data = json.loads(data) 
+        original_text = ''
+        for items in data['rss']['channel']['items']:
+           original_text += ' ' + items['description']
+        return original_text
+ 
+def count_word(original_text):
+
+    to_list = original_text.split(' ')
+    word_value = {}
+    for word in to_list: 
+        if len(word) > 6:
+            if word in word_value:
+                word_value[word] += 1
+            else:
+                word_value[word] = 1
+    return word_value 
+ 
+def sort_top(word_value):
+
+    l = lambda word_value: word_value[1]
+    sort_list = sorted(word_value.items(), key = l, reverse = True)
+    count = 1
+    top_10 = {}
+    for word in sort_list:
+        top_10[count] = word
+        count += 1
+        if count == 10:
+            break
+    return top_10
+ 
+def main():
+    while True:
+        name = input('write work: ')
+        if name == 'work':
+            print('Working ...')
+            top_10 = sort_top(count_word(read_files(name)))
+            for i in top_10.values():
+                print ( i[0])
 
 
-all_items = json_data['rss']['channel']['items']
 
-news_list_json = []
-for i in all_items:
-  news_list_json.append(i['description'].split())
-
-words_list_json = []
-
-for i in news_list_json:
-  for j in i:
-    if len(j)>6:
-     words_list_json.append(j)
-
-words_set_json = set(words_list_json)
-
-my_dict_json = {i:words_list_json.count(i) for i in words_set_json}
-
-pairs_list_json = []
-
-for k,v in zip(my_dict_json.keys(), my_dict_json.values()):
-  pairs_list_json.append((v,k))
-
-words_list_sorted_json = sorted(pairs_list_json, reverse=True)
-
-for i in range(10):
-  print(words_list_sorted_json[i][1])
+main()
 
