37ba175b2260ae4522220338284e4763292cb89d
EgorOnischenko <75452718+EgorOnischenko@users.noreply.github.com>
2021-01-07 09:08:44 +0100

    Update Work_with_XTML.py

--- Work_with_XTML.py
+++ Work_with_XTML.py
@@ -4,34 +4,32 @@
 # In[ ]:
 
 
-def read_files():
+def read_files(fileName):
   import xml.etree.ElementTree as ET
   dict = {}
-  for event, elem in ET.iterparse("txt"):
+  for event, elem in ET.iterparse(fileName):
     s = elem.text
     s = s.split()
-    # print(s)
     for i in s:
       dict[i] = dict.get(i, 0) + 1
-
+  
   return dict
-
-
-def count_word():
-  dict = read_files()
+  
+def count_word(dict, wordLength):
   lst = []
-  for k, v in dict.items():
-    if len(k) > 6:
+  for k,v in dict.items():
+    if len(k) > wordLength:
       lst.append([v, k])
   return lst
 
 
+
 def main():
-  lst = count_word()
+  dict = read_files("txt")
+  lst = count_word(dict, 6)
   lst.sort(reverse=True)
   lst = lst[:10]
   for i in lst:
     print(str(i[1]))
-
 
 main()
